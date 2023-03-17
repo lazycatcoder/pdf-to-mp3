c187511932ad106ccdad1723e398e97cdc7e32b6
Lazycatcoder <lazycatcoder@gmail.com>
2023-03-18 01:18:19 +0200

    add files

---
+++ README.md
@@ -0,0 +1,70 @@
+<div align="center">
+  <h1>PDF to MP3 Converter</h1>
+</div>
+
+<div align="justify">
+  This is a Python script that allows users to convert PDF files to MP3 audio files in the language of their choice. It uses PyPDF2 to extract text from the PDF files, Google Translate API to translate the text into the selected language, and Google Text-to-Speech (gTTS) API to synthesize the speech and save the audio file.
+
+<br>
+
+   The script has a user-friendly command-line interface that prompts the user to select the PDF file, the language, and gives the option to exit or continue converting files. It also creates separate folders for PDF and MP3 files.
+</div>
+
+<div align="center">
+
+   ## 💡Practical use
+
+</div>
+
+<div align="justify">
+  
+  This script can be useful for people who prefer to listen to text instead of reading. This can be convenient, for example, when traveling on public transport or playing sports. In addition, the script can be used to create audio books or audio lectures.
+
+</div>
+
+<br><br>
+
+<div align="center">
+
+# Settings
+To use it, you need to complete the following steps:
+
+<br>
+
+### 📁 Clone this repository
+
+   ```
+   git clone https://github.com/lazycatcoder/pdf-to-mp3.git
+   ```
+
+<br>
+
+### 📦 Install dependencies
+   
+   ```
+   pip install -r requirements.txt
+   ```
+
+
+# 
+
+## ✨ How to use
+
+<div align="justify">
+  
+- Download the repository and install the required modules
+- Create "pdf" and "mp3" folders in the same directory as the script (optional)
+- Place the PDF files to be converted into the "pdf" folder
+- Run the script 🚀
+- Follow the instructions on the screen
+
+</div>
+
+<br>
+
+### 🔧 Additional Information
+<div align="justify">
+
+🔴 For the script to work correctly, it is necessary that PDF files be recognized or that it is possible to highlight the characters contained in them, since this is necessary for the script to read text from files.
+
+</div>
---
+++ pdf-to-mp3.py
@@ -0,0 +1,142 @@
+# this script converts pdf files to mp3 in the language of your choice
+
+import os
+import glob
+import PyPDF2
+from googletrans import Translator, LANGUAGES
+from gtts import gTTS
+
+
+def PdfConverter():
+    while True:
+        
+        # Create an mp3 folder if it doesn't exist
+        def create_mp3_folder():
+            current_dir = os.getcwd()
+
+            mp3_folder_path = os.path.join(current_dir, 'mp3')
+            if not os.path.exists(mp3_folder_path):
+                os.makedirs(mp3_folder_path)
+
+        # Create a pdf folder if it doesn't exist
+        def create_pdf_folder():
+            current_dir = os.getcwd()
+            
+            pdf_folder_path = os.path.join(current_dir, 'pdf')
+            if not os.path.exists(pdf_folder_path):
+                os.makedirs(pdf_folder_path)
+
+        # Get a list of PDF files in the pdf folder
+        def get_pdf_files():
+            pdf_files = glob.glob('pdf/*.pdf')
+            
+            if len(pdf_files) == 0:
+                print('No pdf files to convert')
+                return None
+
+            # List files
+            print('List of PDF files:')
+            for i, file_path in enumerate(pdf_files):
+                file_name = os.path.basename(file_path)
+                print(f'{i+1}. {file_name}')
+            
+            return pdf_files
+
+
+        # We get the file that needs to be converted and convert it according to the selected language
+        def get_pdf_file_and_convert(pdf_files):
+            # Get the number of the file to be converted
+            while True:
+                file_number = input('Enter PDF file number to convert to MP3 or enter "0" to exit: ')
+                try:
+                    file_number = int(file_number)
+                    if file_number == 0:
+                        return None
+                    if file_number < 1 or file_number > len(pdf_files):
+                        print(f'The number must be from 1 to {len(pdf_files)}')
+                    else:
+                        return pdf_files[file_number-1]
+                except ValueError:
+                    print('Enter an integer')
+
+        # Create a pdf folder if it hasn't already been created
+        create_pdf_folder()
+
+        # Create an mp3 folder if it hasn't already been created
+        create_mp3_folder()
+
+        # Get a list of PDFs
+        pdf_files = get_pdf_files()
+        if pdf_files is None:
+            print("End of the program.")
+            exit()
+
+        # Get the number of the file to be converted
+        pdf_file_to_convert = get_pdf_file_and_convert(pdf_files)
+        if pdf_file_to_convert is None:
+            print("End of the program.")
+            exit()
+
+        # Getting the playback language
+        print('Select playback language: 1. English, 2. Polish, 3. Ukrainian, 4. Keep original.')
+
+        while True:
+            language_choice = input('Enter language number: ')
+            if language_choice in ['1', '2', '3', '4']:
+                break
+            print('Incorrect choice. Try again.')
+        language_map = {'1': 'en', '2': 'pl', '3': 'uk'}
+        language = language_map[language_choice] if language_choice != '4' else None
+
+
+        # Open PDF file
+        pdf_file = open(pdf_file_to_convert, 'rb')
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        # Initialize the text object
+        text = ''
+        
+        # Iterate through all the pages of the PDF file and get the text
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num].extract_text()
+            text += page.strip().replace("\n", " ").replace(" ' ", "'")
+
+        # Translate text if needed
+        if language:
+            translator = Translator()
+            translation = translator.translate(text, dest=language)
+            text = translation.text
+        # Recognize text and get language code
+        elif language == None:
+            translator = Translator()
+            detected = translator.detect(text)
+            lang_code = detected.lang
+            language = lang_code 
+            
+        # Synthesizing speech and saving the audio file
+        print("The file is being written. Waitin...")
+        tts = gTTS(text=text, lang=language, slow=False)
+        
+        mp3_file_name = os.path.splitext(os.path.basename(pdf_file_to_convert))[0] + '.mp3'
+        mp3_file_path = os.path.join('mp3', mp3_file_name)
+
+        tts.save(mp3_file_path)
+        print(f'{mp3_file_name} file successfully created in mp3 folder.\n')
+        
+        # Selection result
+        while True:
+            try:
+                choice_result = int(input('Make a choice: 1. Continue, 2. Exit: '))
+                if choice_result == 1:
+                    break
+                elif choice_result == 2:
+                    print("End of the program.")
+                    exit()
+            except ValueError:
+                print("Invalid number entered. Try again.")
+
+        continue
+
+
+if __name__ == '__main__':
+    PdfConverter()
